2f9f741b78e87f4e6efe127235a63df36ceea319
marcinkordacki <marcin.kordacki@ded.com.pl>
2017-07-30 17:54:18 +0200

    Added new Js project files

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by marcinkordacki on 30/07/17.
+ */
+
+function createParagraph() {
+    var para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+}
+
+var buttons = document.querySelectorAll('button');
+
+for (var i = 0; i < buttons.length ; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+}
+
